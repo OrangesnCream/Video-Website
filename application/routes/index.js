c8568cf71d3ea7107f1adef62a9ffa43066aee9c
unknown <lalvarado.rivas3134@gmail.com>
2023-04-20 23:54:21 -0700

    maybe milestone 2,might need more

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,21 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Luis Alvarado Rivas" });
+  res.render('index', { title: 'CSC 317 App', pageTitle: 'CSC 317 Website',name:"Luis Alvarado Rivas" });
 });
+
+
 router.get("/login",function(req,res){
-  res.render('login');
+  res.render('login',{pageTitle: 'Login'});
 });
 router.get("/postvideo",function(req,res){
-  res.render('postvideo');
+  res.render('postvideo',{pageTitle: 'Upload Video'});
 });
 router.get("/profile",function(req,res){
-  res.render('profile');
+  res.render('profile',{pageTitle: 'Profile'});
 });
 router.get("/registration",function(req,res){
-  res.render('registration');
+  res.render('registration',{pageTitle: 'Register'});
 });
 router.get("/viewpost",function(req,res){
-  res.render('viewpost');
+  res.render('viewpost',{pageTitle: 'User Post'});
 });
+
 module.exports = router;

35d079f81e11f2f6b9d621b3ac9b9a46d232c0c1
OrangesnCream <lalvarado.rivas3134@google.com>
2023-04-20 21:11:33 -0700

    tring to do milestone 2

--- application/routes/index.js
+++ application/routes/index.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Luis Alvarado Rivas" });
 });
-
+router.get("/login",function(req,res){
+  res.render('login');
+});
+router.get("/postvideo",function(req,res){
+  res.render('postvideo');
+});
+router.get("/profile",function(req,res){
+  res.render('profile');
+});
+router.get("/registration",function(req,res){
+  res.render('registration');
+});
+router.get("/viewpost",function(req,res){
+  res.render('viewpost');
+});
 module.exports = router;

aad7af070b2b59de2bba88ccc8a620880ede7d21
unknown <lalvarado.rivas3134@gmail.com>
2023-05-13 21:45:56 -0700

    pain is the french word for bread, vs code is a bakery

--- application/routes/users.js
+++ application/routes/users.js
@@ -12,4 +12,31 @@ try{
 }
 });
 
+router.post('/registration',async function(req,res,next){
+ var {username,Email,password}=req.body;
+  try{
+    var [rows,fields]= await db.execute(`select id from users where username=?;`,[username]);
+    if(rows&&rows.length>0){
+      return res.redirect('/registration');
+    }
+    var [rows,fields]= await db.execute(`select id from users where email=?;`,[Email]);
+    if(rows&&rows.length>0){
+      return res.redirect('/registration');
+    }
+    var [resultObject,fields]= await db.execute(`INSERT INTO users
+     (username,email,password) value (?,?,?);`,[username,Email,password]);
+
+     if(resultObject&&resultObject.affectedRows==1){
+      return res.redirect("/login");
+     }else{
+      return res.redirect("/registration");
+     }
+  }catch(error){
+    next(error);
+
+  }
+
+
+});
+
 module.exports = router;
